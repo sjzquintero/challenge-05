4bed347a58a3f707eae13e67b9f81e16f6cbf1e0
Santiago Jimenez <squintero@rrc.ca>
2024-06-11 21:40:44 -0500

    created and fille the products.csv, also rewrote the seeds.rb to pull the product and category data from the .csv file

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,24 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'faker'
+require 'csv'
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product['category_name'])
+  category.products.create(
+    title: product['product_name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock_quantity']
   )
 end
 
-puts "Created 676 products."
+puts "Products and categories seeded successfully."
