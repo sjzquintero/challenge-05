973f8a7260f0433ad7588f1af830bbbfec9e83ca
Santiago Jimenez <squintero@rrc.ca>
2024-06-11 21:06:17 -0500

    eliminated the 676 products created in part 1, and added the foreign key to product and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_015658) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_020428) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_015658) do
     t.integer "stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
