413f1c0148b8150fac1ecc18ca974ea6f50f2a6b
Santiago Jimenez <squintero@rrc.ca>
2024-06-11 20:57:32 -0500

    generated the new model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_233643) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_015658) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
